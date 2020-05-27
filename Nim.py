55619f465f65de1224804cfd6b503a9e046cfc17
Aman Rauniyar <36833312+amanrauniyar@users.noreply.github.com>
2020-05-26 23:03:46 -0400

    Add files via upload

--- Nim.py
+++ Nim.py
@@ -1,15 +1,42 @@
-play_str=input("Would you like to play? (0=no, 1=yes) ")
+import math
+import random
+rand = random.randint(1, 3)
+
+play_str=input("Would you like to play the Game of Nim? (0=no, 1=yes) ")
 
 human = int(0)     # Initialize the number of times humans win the game
 computer = int(0)  # Initialize the number of times computer win the game
 
-while int(play_str) != 0:  # use while to determine if the game starts
+def minimax(curDepth, nodeIndex,
+            maxTurn, scores,
+            targetDepth):
+
+    if (curDepth == targetDepth):
+        return scores[nodeIndex]
+
+    if (maxTurn):
+        return max(minimax(curDepth + 1, nodeIndex * 2,
+                           False, scores, targetDepth),
+                   minimax(curDepth + 1, nodeIndex * 2 + 1,
+                           False, scores, targetDepth))
+
+    else:
+        return min(minimax(curDepth + 1, nodeIndex * 2,
+                           True, scores, targetDepth),
+                   minimax(curDepth + 1, nodeIndex * 2 + 1,
+                           True, scores, targetDepth))
+
+
+scores = [1,2,3]
+
+treeDepth = math.log(len(scores), 2)
+while int(play_str) != 0:
 
     pile1 = int(12)
     pile2 = int(12)
     print("Start --> Pile 1:",pile1,"   Pile 2:",pile2)
 
-    while (pile1 > 0) or (pile2 > 0):  # use while to determine if the pile is empty.
+    while (pile1 > 0) or (pile2 > 0):
         pile = int(input("Choose a pile (1 or 2): "))
 
         while ((pile == 1)and(pile1 == 0))or((pile == 2)and(pile2 == 0)):
@@ -20,8 +47,8 @@ while int(play_str) != 0:  # use while to determine if the game starts
 
         else:
 
-            while (pile != 1) and (pile != 2):
-                # If the number of the pile is not legal. Can't continue.
+            while (pile != 1) and (pile != 2): # If the number of the pile is not legal. Can't continue.
+
 
                 print("Pile must be 1 or 2 and non-empty. Please try again.")
                 pile = int(input("Choose a pile (1 or 2): "))
@@ -30,7 +57,7 @@ while int(play_str) != 0:  # use while to determine if the game starts
                 stones = int(input("Choose stones to remove from pile: "))
 
                 while (stones > 3) or (stones < 1):
-                    # If the number of the stones is not beween 1 and 3. Can't continue.
+
 
                     print("Invalid number of stones. Please try again.")
                     stones = int(input("Choose stones to remove from pile: "))
@@ -53,22 +80,22 @@ while int(play_str) != 0:  # use while to determine if the game starts
                             print("Score -> human:",human,"; computer:",computer)
 
                         else:
-                            # The stones was not taken by humans.
+
 
                             if (pile2 > 0):
-                                # If the pile2 is not taken empty.
 
-                                pile2 = pile2 - 1
+
+                                pile2 = pile2 - rand
                                 print("Computer -> ",end='')
-                                print("Remove 1 stones from pile 2")
+                                print("Removed stones from pile 2")
                                 print("Pile 1:",pile1,"   Pile 2:",pile2)
 
                             else:
-                                # If the pile2 is taken empty.
 
-                                pile1 = pile1 - 1
+
+                                pile1 = pile1 - rand
                                 print("Computer -> ",end='')
-                                print("Remove 1 stones from pile 1")
+                                print("Removed stones from pile 1")
                                 print("Pile 1:",pile1,"   Pile 2:",pile2)
 
                             if (pile1 <= 0) and (pile2 <= 0):
@@ -97,29 +124,29 @@ while int(play_str) != 0:  # use while to determine if the game starts
                             if (pile1 > 0):
                                 # If the pile1 is not taken empty.
 
-                                pile1 = pile1 - 1
+                                pile1 = pile1 - rand
                                 print("Computer -> ",end='')
-                                print("Remove 1 stones from pile 1")
+                                print("Removed stones from pile 1")
                                 print("Pile 1:",pile1,"   Pile 2:",pile2)
 
                             else:
                                 # If the pile1 is taken empty.
 
-                                pile2 = pile2 - 1
+                                pile2 = pile2 - rand
                                 print("Computer -> ",end='')
-                                print("Remove 1 stones from pile 2")
+                                print("Removed stones from pile 2")
                                 print("Pile 1:",pile1,"   Pile 2:",pile2)
 
                             if (pile1 <= 0) and (pile2 <= 0):
                                 # The stones was taken empty by computer.
 
-                                computer = computer + 1  # Computer win the game plus one.
+                                computer = computer + 1
                                 print("\nComputer wins!")
                                 print("Score -> human:",human,"; computer:",computer)
 
     else:
         play_str = input("\nWould you like to play again? (0=no, 1=yes) ")
-        # Judge whether to come again
+
 
 else:
    print("\nThanks for playing! See you again soon!")
